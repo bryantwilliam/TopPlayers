62610acb6eb2b1f385de78ae887824409b8de9f9
gogobebe2 <gogobebe2@gmail.com>
2015-08-01 10:42:41 +1200

    1.1.1-DEVELOPMENT-SNAPSHOT

--- src/main/java/com/gmail/gogobebe2/topplayers/SignUpdater.java
+++ src/main/java/com/gmail/gogobebe2/topplayers/SignUpdater.java
@@ -2,8 +2,12 @@ package com.gmail.gogobebe2.topplayers;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.SkullType;
+import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Sign;
+import org.bukkit.block.Skull;
 
 import java.util.concurrent.TimeUnit;
 
@@ -32,7 +36,7 @@ public class SignUpdater implements Runnable {
         }
     }
 
-    protected static void updateSign(Sign sign, int placement) {
+    protected void updateSign(Sign sign, int placement) {
         Record record = Record.getRecord(placement, sign.getWorld().getUID());
         String name;
         long time;
@@ -47,8 +51,37 @@ public class SignUpdater implements Runnable {
 
         sign.setLine(0, ChatColor.DARK_BLUE + "Top Player:");
         sign.setLine(1, ChatColor.GOLD + name);
-        sign.setLine(2, ChatColor.BLUE + "Placed at: " + placement);
+        sign.setLine(2, ChatColor.BLUE + "Placed " + ordinal(placement));
         sign.setLine(3, ChatColor.GREEN + "With " + time + " hours");
         sign.update();
+
+        int radius = plugin.getConfig().getInt("head identification block radius");
+        for (int x = sign.getX() - radius; x < sign.getX() + radius; x++) {
+            for (int y = sign.getY() - radius; y < sign.getY() + radius; y++) {
+                for (int z = sign.getZ() - radius; z < sign.getZ() + radius; z++) {
+                    Block block = sign.getWorld().getBlockAt(x, y, z);
+                    if (block.getType() == Material.SKULL) {
+                        Skull skull = (Skull) block;
+                        skull.setSkullType(SkullType.PLAYER);
+                        skull.setOwner(name);
+                        skull.update();
+                    }
+                }
+            }
+        }
+    }
+
+    private String ordinal(int i) {
+        int mod100 = i % 100;
+        int mod10 = i % 10;
+        if(mod10 == 1 && mod100 != 11) {
+            return i + "st";
+        } else if(mod10 == 2 && mod100 != 12) {
+            return i + "nd";
+        } else if(mod10 == 3 && mod100 != 13) {
+            return i + "rd";
+        } else {
+            return i + "th";
+        }
     }
 }
